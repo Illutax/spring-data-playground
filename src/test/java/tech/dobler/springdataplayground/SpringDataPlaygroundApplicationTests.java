da7e923e8370086cd730c0e7ae290381a98d06bd
Valerij Dobler <vd@wps.de>
2024-04-10 23:02:23 +0200

    Init

---
+++ src/test/java/tech/dobler/springdataplayground/SpringDataPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dobler.springdataplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
